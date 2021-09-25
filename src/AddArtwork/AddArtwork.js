523de5c56781dcbfe4fd5c0b38ffa3237ec6890b
Todd Albert <todd@bocacode.com>
2021-09-25 19:44:17 -0400

    uploading in progress

--- src/AddArtwork/AddArtwork.js
+++ src/AddArtwork/AddArtwork.js
@@ -1,12 +1,24 @@
-import React, { useState, useContext } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { uploadFile } from '../utils'
 import './style.css'
 
-
 function AddArtwork(){
-    let history = useHistory()
-    return(<p>AddArtwork</p>)
-    
+    const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+        onDrop: files => {
+            // console.log(files)
+            files.forEach(file => uploadFile(file))
+        }
+    })
+  
+    return (
+      <section className="container">
+        <div {...getRootProps({className: 'dropzone'})}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+      </section>
+    )
 }
 
 export default AddArtwork
